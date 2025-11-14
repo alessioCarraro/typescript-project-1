2d1e2ae7575a99eb814101cc84fe07c05261e04e
Alessio Carraro <alessiocarraro2005@gmail.com>
2025-11-14 09:45:45 +0100

    eserciziCalcolatrice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,71 @@
-/* eslint-disable no-console */
+/*
+------------
+CALCOLATRICE
+------------
 
-function main(): void
+scrivi un programma che chieda all'utente di 
+inserire un operatore numerico ('+', '-', '/', '*'),
+il primo valore numerico e il secondo valore numerico
+e che restituisca il risultato dell'operazione matematica a schermo
+
+usare 'prompt' e 'alert'
+*/
+
+const operatore = prompt("inserire un operatore tra: \n" +
+    "' + ' \n" +
+    "' - '\n" +
+    "' * '\n" +
+    "' / '");
+
+if (!(operatore == "+" || operatore == "-" || operatore == "/" || operatore == "*"))
 {
-    console.log("Hello, world!");
+    alert("errore, non hai inserito uno degli operatori richiesti");
 }
+else
+{
+    const numero1 = Number(prompt("inserire il primo numero"));
+    if (Number.isNaN(numero1))
+    {
+        alert("non hai inserito un numero");
+    }
 
-main();
+    const numero2 = Number(prompt("inserire il secondo numero"));
+    if (Number.isNaN(numero2))
+    {
+        alert("non hai inserito un numero");
+    }
+
+    let risultato = NaN;
+    if (operatore == "+")
+    {
+        risultato = numero1 + numero2;
+    }
+    else if (operatore == "-")
+    {
+        risultato = numero1 - numero2;
+    }
+    else if (operatore == "*")
+    {
+        risultato = numero1 * numero2;
+    }
+    else if (operatore == "/")
+    {
+        if (numero2 == 0)
+        {
+            alert("non puoi dividere per 0");
+        }
+        else
+        {
+            risultato = numero1 / numero2;
+        }
+    }
+    else
+    {
+        alert("non hai inserito un operatore");
+    }
+
+    if (!Number.isNaN(risultato))
+    {
+        alert("risultato: " + risultato);
+    }
+}
